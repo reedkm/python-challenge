dc66e36a3acdafa43a3bd39e0172c653fabf7416
reedkm <kreed@mac.com>
2018-08-22 11:29:58 -0400

    Revised to penultimate version; cleaned up

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,43 +14,53 @@ with open(budget_data_csv, newline='') as csvfile:
 	# CSV reader specifies delimiter and variable that holds csv contents
 	csvreader = csv.reader(csvfile, delimiter=',')
 	header = next(csvreader, None)
-	#budget_list = list(csvreader)
-	
+	#budget_list = list(csvreader) 
+
 	# Variable to create tuple from each row
 	budget_list = [[row[0], int(row[1])] for row in csvreader]
-	#print(budget_list)
-	
+	#print(budget_list) 
+
 	# Use list comprehension to read in budget list
 	profits = [profit for (month, profit) in budget_list]
 	#print("----------------------------")
-	#print(profits)
-	
+	#print(profits) 
+
 	# Use len to count rows for total months
-	totalMonths = len(budget_list)
-	
+	totalMonths = len(budget_list) 
+
 	# Sum the rows for total profit
-	total = sum(profits)
-	
+	total = sum(profits) 
+
 	# Average the profits using the total divided by total months, rounded to 2 places
-	avgChange = round(total/totalMonths, 2)
-	
+	avgChange = round(total/totalMonths, 2) 
+
 	# Check max and min increase to compare for testing
 	maxIncrease = max(profits)
-	minDecrease = min(profits)
-	
+	minDecrease = min(profits) 
+
 	# Use one-line function to get the max of the second index value
 	maxMonth = max(budget_list, key=lambda x: x[1])
 
 	# Use one-line function to get the min of the second index value
 	minMonth = min(budget_list, key=lambda x: x[1])
-		
+	 
 #
 # Main block to print out results
 print("Financial Analysis")
 print("----------------------------")
-print(f"Total Months: {totalMonths}")					# Use string on variable, concatenate with text
-print(f"Total: $ {total}")									# Use f-string to simplify with total
-print(f"Average Change: $ {avgChange}")						# Use f-string again for average change
-print("Greatest Increase in Profits: " + maxMonth[0] + " ($" + str(maxMonth[1]) + ")")		#
-print("Greatest Decrease in Profits: " + minMonth[0] + " ($" + str(minMonth[1]) + ")")		#
+
+# Use string on variable, concatenate with text
+print(f"Total Months: {totalMonths}")
+
+# Use f-string to simplify with total
+print(f"Total: $ {total}")
+
+# Use f-string again for average change
+print(f"Average Change: $ {avgChange}")
+
+# Use f-string again for increase; pull out indexes of variable to display as needed
+print(f"Greatest Increase in Profits: {maxMonth[0]} (${maxMonth[1]})")
+
+# Use f-string again for decrease; pull out indexes of variable to display as needed
+print(f"Greatest Decrease in Profits: {minMonth[0]} (${minMonth[1]})")
 
