7c4711e373e8bf580fe15d72e762c4d5749a1efc
reedkm <kreed@mac.com>
2018-08-24 16:57:42 -0400

    Added loop for candidates after using unique function

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -19,46 +19,55 @@ with open(poll_data_csv, newline='') as csvfile:
 
 	# Variable to create tuple from each row
 	votes = [[row[0], row[1], row[2]] for row in csvreader]
-	#print(votes) 
+	#print(votes)
 
-	# Use list comprehension to read in voter ids
+	# Use list comprehension to read in voter ids and candidates
 	voter_ids = [voter_ids for (voter_ids, county, candidate) in votes]
+	candidates = [candidate for (voter_ids, county, candidate) in votes]
 
 	# Use len to count rows for voter ids; assumes no duplicates
 	totalVotes = len(voter_ids) 
-	print(totalVotes)
 	
-	CorreyVotes = []
-	# Loop through looking for the votes
-	for row in votes:
-		if row[2] == "Correy":
-				CorreyVotes.append(1)
-		totalCVotes = len(CorreyVotes)
-	print(totalCVotes)
+	# Main block to print out results
+	print("Election Results")
+	print("----------------------------")
+	print(f"Total Votes: {totalVotes}")
+	print("----------------------------")
 	
-	KhanVotes = []
-	# Loop through looking for the votes
-	for row in votes:
-		if row[2] == "Khan":
-				KhanVotes.append(1)
-		totalKVotes = len(KhanVotes)
-	print(totalKVotes)
-	
-	LiVotes = []
-	# Loop through looking for the votes
-	for row in votes:
-		if row[2] == "Li":
-				LiVotes.append(1)
-		totalLVotes = len(LiVotes)
-	print(totalLVotes)
+	# Function code for unique values from web
+	def unique(list):
 
-	OTooleyVotes = []
-	# Loop through looking for the votes
-	for row in votes:
-		if row[2] == "O'Tooley":
-				OTooleyVotes.append(1)
-		totalOVotes = len(OTooleyVotes)
-	print(totalOVotes)
+	# intilize a null list
+		unique_list = []
+	
+		# traverse for all elements
+		for x in list:
+			# check if exists in unique_list or not
+			if x not in unique_list:
+				unique_list.append(x)
+		# print list
+		#for x in unique_list:
+			#print(x),
+		#print(unique_list)
+		
+		for item in unique_list:
+		
+			CandidateVotes = []
+			# Loop through looking for the votes
+			for row in votes:
+				if row[2] == item:
+					CandidateVotes.append(1)
+			totalCVotes = len(CandidateVotes)
+			percentage = round(totalCVotes/totalVotes * 100,3)
+			
+			print(f"{item}: {percentage}% ({totalCVotes})")		
+		
+	list = candidates
+	unique(list)
+	
+	print("----------------------------")
+	print(f"Winner: ")
+	print("----------------------------")
 
 # save the output file path
 #output_file = os.path.join("output.csv")
