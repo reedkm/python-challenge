9dcadfa94136fe21294d354adb250f2cf1c451c1
Kenneth Reed <kenneth@Kenneths-iMac.local>
2018-08-25 16:45:28 -0400

    Final: cleaned and tested

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,7 +14,7 @@ with open(poll_data_csv, newline='') as csvfile:
 	# CSV reader specifies delimiter and variable that holds csv contents
 	csvreader = csv.reader(csvfile, delimiter=',')
 	header = next(csvreader, None)
-	#poll_list = list(csvreader) 
+	#poll_list = list(csvreader)
 
 	# Variable to create tuple from each row
 	votes = [[row[0], row[1], row[2]] for row in csvreader]
@@ -25,45 +25,45 @@ with open(poll_data_csv, newline='') as csvfile:
 	candidates = [candidate for (voter_ids, county, candidate) in votes]
 
 	# Use len to count rows for voter ids; assumes no duplicates
-	totalVotes = len(voter_ids) 
-	
+	totalVotes = len(voter_ids)
+
 	# Main block to print out results
 	print("Election Results")
 	print("----------------------------")
 	print(f"Total Votes: {totalVotes}")
 	print("----------------------------")
-	
 
-	# save the output file path
-	output_file = os.path.join("output.txt")
+	# Save the output file path
+	output_file = os.path.join("election_results.txt")
 	file = open(output_file, "w")
-	
+
+	# Main block to save results to text
 	file.write("Election Results\n")
 	file.write("----------------------------\n")
 	file.write(f"Total Votes: {totalVotes}\n")
 	file.write("----------------------------\n")
-	
-	# Function code for unique values from web
+
+	# Function code for unique values from web; use to find distinct candidates
 	def unique(list):
 
-	# intilize a null list
+	# Set empty list
 		unique_list = []
 
-		# traverse for all elements
+		# Loop to check all items in list
 		for x in list:
-			# check if exists in unique_list or not
+			# check if candidate already exists; if not append
 			if x not in unique_list:
 				unique_list.append(x)
-		# print list
-		#for x in unique_list:
-			#print(x),
-		#print(unique_list)
 
+		# Set empty list to contain distinct candidates
 		summary = []
+		# Set empty list to contain candidate vote totals
 		sumB = []
 		for item in unique_list:
+			# Set empty list to contain votes to count
 			CandidateVotes = []
 			summary.append(item)
+	
 			# Loop through looking for the votes
 			for row in votes:
 				if row[2] == item:
@@ -71,29 +71,32 @@ with open(poll_data_csv, newline='') as csvfile:
 			totalCVotes = len(CandidateVotes)
 			percentage = round(totalCVotes/totalVotes * 100, 3)
 
-			
+			# Print out each candidate, percentage, and their vote totals
 			print(f"{item}: {percentage}% ({totalCVotes})")
 			sumB.append(totalCVotes)
 
+			# Write out each candidate results to text
 			file.write(f"{item}: {percentage}% ({totalCVotes})\n")
 
-		#print(summary)
+		# zip candidate and their votes together to look for winner
 		zipped = zip(summary, sumB)
-		#map(max, zip(zipped))
 
-		#print(max(sumB))
+		# For loop to check for winner
 		for name, number in zipped:
 			if number == max(sumB):
 				winner = name
+		
+		# Print out winner
 		print("----------------------------")
 		print(f"Winner: {winner}")
 		print("----------------------------")
 
+		# Write out winner to file
 		file.write("----------------------------\n")
 		file.write(f"Winner: {winner}\n")
 		file.write("----------------------------\n")
 
+		# Close file
 		file.close()
 
-	#list = candidates
 	unique(candidates)
